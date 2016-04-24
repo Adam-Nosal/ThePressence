93cc9a63c2318389b5eba95a34a2d93a533c195d
bsliwa <b.sliwa@lgbs.pl>
2016-04-24 15:56:07 +0200

    initial comit

---
+++ Unity/Assets/Scripts/CirclesGame/CirclesController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class CirclesController : MonoBehaviour, iController {
+    public void Init()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnClose()
+    {
+        throw new NotImplementedException();
+    }
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Unity/Assets/Scripts/CirclesGame/CirclesView.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CirclesView : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
