c173caf3f0088dbe6706ddc8954467d1bfaad059
bsliwa <b.sliwa@lgbs.pl>
2016-04-25 00:01:33 +0200

    circles game work

--- Unity/Assets/Scripts/CirclesGame/CirclesController.cs
+++ Unity/Assets/Scripts/CirclesGame/CirclesController.cs
@@ -3,6 +3,14 @@ using System.Collections;
 using System;
 
 public class CirclesController : MonoBehaviour, iController {
+
+
+    private float rotationAngle;
+    public DifficultyEnum difficulty;
+    public GameObject[] circles;
+    public GameObject winImage;
+    
+    
     public void Init()
     {
         throw new NotImplementedException();
@@ -15,16 +23,88 @@ public class CirclesController : MonoBehaviour, iController {
 
     // Use this for initialization
     void Start () {
-	
+        switch (difficulty)
+        {
+            case DifficultyEnum.EASY:
+                rotationAngle = 60f;
+                RandomizePairs(1);
+                break;
+            case DifficultyEnum.MEDIUM:
+                rotationAngle = 30f;
+                RandomizePairs(2);
+                break;
+            case DifficultyEnum.HARD:
+                rotationAngle = 15f;
+                RandomizePairs(2);
+                break;
+        }
+
+        PrepareCircles();
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    private void RandomizePairs(int pairsNumber)
+    {
+        System.Random rnd = new System.Random((int)DateTime.Now.Ticks);
+        for (int i = 0; i < pairsNumber; i++)
+        {
+            bool pairFound = false;
+            while (!pairFound)
+            {
+                int firstCirlce = rnd.Next(0, 4);
+                int secondCircle = firstCirlce;
+                while(firstCirlce == secondCircle)
+                {
+                    secondCircle = rnd.Next(0, 4);
+                    if (firstCirlce != secondCircle)
+                    {
+                        circles[firstCirlce].GetComponent<Tuple>().tupleCircle = circles[secondCircle];
+                    }
+                }
+            }
+        }
+    }
+
+    private void PrepareCircles()
+    {
+        System.Random rnd = new System.Random((int)DateTime.Now.Ticks);
+        int rotationsNumber;
+        foreach (GameObject circle in circles)
+        {
+            rotationsNumber = rnd.Next(1, 20);
+            for(int i =0; i< rotationsNumber; i++)
+            {
+                RotateCircle(circle);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
 	
 	}
 
-    public void RotateCircle(RectTransform circle)
+    public void RotateCircle(GameObject circle)
     {
-        circle.Rotate(new Vector3(0, 0, 90));
+        circle.GetComponent<RectTransform>().Rotate(new Vector3(0, 0, rotationAngle));
+        if (circle.GetComponent<Tuple>().tupleCircle != null)
+        {
+            for(int i =0; i<3; i++)
+            {
+                GameObject pairedCircle = circle.GetComponent<Tuple>().tupleCircle.gameObject;
+                RotateCircle(pairedCircle);
+            }
+        }
+        CheckWinningConditions();
+    }
+
+    private void CheckWinningConditions()
+    {
+        if(circles[0].GetComponent<RectTransform>().eulerAngles.z== circles[1].GetComponent<RectTransform>().eulerAngles.z
+            && circles[0].GetComponent<RectTransform>().eulerAngles.z == circles[2].GetComponent<RectTransform>().eulerAngles.z
+            && circles[0].GetComponent<RectTransform>().eulerAngles.z == circles[3].GetComponent<RectTransform>().eulerAngles.z
+            && circles[0].GetComponent<RectTransform>().eulerAngles.z == circles[4].GetComponent<RectTransform>().eulerAngles.z)
+        {
+            winImage.SetActive(true);
+        }
     }
 }
---
+++ Unity/Assets/Scripts/CirclesGame/Tuple.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tuple : MonoBehaviour {
+    public GameObject tupleCircle;
+}
